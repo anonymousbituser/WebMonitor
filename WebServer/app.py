30d91c26e7ff3a27cc77963f65433a3d1c7d9f28
anonymousbituser <86840853+anonymousbituser@users.noreply.github.com>
2021-07-02 15:07:32 -0400

    Update app.py
    Add route function to display/start flask handle

--- WebServer/app.py
+++ WebServer/app.py
@@ -1,28 +1,34 @@
 from flask import Flask
 import time
 
+app = Flask(__name__)  # Class handle to call Flask API
+@app.route('/')  # Decorator function - wraps a function for Flask to operate - maps url to return value
+def main_page():
+    return 'Chilly Dog Web Monitor'
 
-class FlaskClass(object):
-    def __init__(self):
-        self.appHandle = Flask(__name__)
 
-    def start_Flask(self):
-        # @self.appHandle.route("/")
-        return "Test"
+
+# class FlaskClass(object):
+#     def __init__(self):
+#         self.appHandle = Flask(__name__)
+#
+#     def start_Flask(self):
+#         # @self.appHandle.route("/")
+#         return "Test"
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    app = FlaskClass()
-    @app.appHandle.route("/")
-    def hello():
-        return "This is a test"
+    app.run(debug=True)  # Main entry point - start web server
+    # app = FlaskClass()
+    # @app.appHandle.route("/")
+    # def hello():
+    #     return "This is a test"
+    #
+    # app.start_Flask()
+    # hello()
+    # while(True):
+    #     time.sleep(1)
+    # print("End")
 
-    app.start_Flask()
-    hello()
-    while(True):
-        time.sleep(1)
-    print("End")
 
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
