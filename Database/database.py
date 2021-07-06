c1c8028c3093b5c8e69a0e8b6097a15d0151b92b
anonymousbituser <86840853+anonymousbituser@users.noreply.github.com>
2021-07-06 04:37:19 -0400

    Update database.py
    Can now correctly store the actual dataset value (6 timestamps and 6 temp values in json string format) into sqlite database table.

--- Database/database.py
+++ Database/database.py
@@ -14,9 +14,7 @@ class Database(object):
     def setup(self):  # Configure database
         # Create tables/setup database  # TODO: Check to see if database exists - if so, do not execute
         try:
-            self.cur.execute("""CREATE TABLE sensor_data ( 
-                                temp_data text
-                                )""")
+            self.cur.execute('CREATE TABLE IF NOT EXISTS sensor_data (temp_data text)')
             self.conn.commit()  # Push changes to database
             self.setup_database = True  # Flag to ensure database has been properly setup
         except sqlite3.OperationalError as e:
@@ -24,8 +22,9 @@ class Database(object):
 
     def store_data(self):
         dataset = self.get_dataset()
+        # dataset = '74.5'
         print(dataset)
-        self.cur.execute("INSERT INTO sensor_data VALUES ('?')", dataset)  # Store actual data
+        self.cur.execute("INSERT INTO sensor_data (temp_data) VALUES (?)", (dataset,))  # Store actual data
         self.conn.commit()  # Push changes to database
 
     def get_dataset(self):  # Grabs dataset values from temp_handler
@@ -35,12 +34,18 @@ class Database(object):
         dataset_values = self.temp_handle.create_graph_dataset()
         return dataset_values
 
-    def close(self):  # Deallocate any resources/close database
-        if self.conn:  # If connection, close database
-            self.conn.close()
+    def close_db(self):  # Deallocate any resources/close database
+        if self.conn or self.cur:  # If connection, close database
+            try:
+                self.conn.close()
+                self.cur.close()
+            except Exception as e:
+                print(e)
 
 
 # Used to for testing/troubleshooting purposes
 if __name__ == '__main__':
     db = Database()
+    db.setup()
     db.store_data()
+    db.close_db()
